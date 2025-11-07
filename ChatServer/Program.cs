bfb579318445b0dad5e896443e0cb560b949c619
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-07 14:37:01 +0100

    add userID counter, and users dict to store username as key and User-object as value

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,5 +1,16 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Shared;
+using ChatServer.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+int userID = 0;
+
+// Dictionary keyed by Username
+var users = new Dictionary<string, User>
+{
+    { "TestUser", new User { Id = 1, Password = "Password123" } }
+};
 
 app.MapGet("/", () => "Hello world");
 
