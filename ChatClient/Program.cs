c1135b49b05d591cc44b23fe6c63ffeb85de2335
aponten <andyponten@gmail.com>
2025-11-07 11:37:35 +0100

    Add textbox for testing

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -10,13 +10,21 @@ internal static class Program
     {
         Raylib.InitWindow(800, 480, "DiscLord 1.0.0");
 
+        int x = 200;
+        int y = 200;
+        int width = 150;
+        int height = 80;
+
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.White);
 
             Raylib.DrawText("Hej Mackan!", 12, 12, 20, Color.Black);
-            Raylib.DrawRectangle(x, y, width, height, Color.Black);
+            // Textruta test
+            Raylib.DrawRectangle(x, y, width, height, Color.DarkGray);
+            Raylib.DrawText("Logga in", x + 20, y + 25, 20, Color.White);
+
 
             Raylib.EndDrawing();
         }
