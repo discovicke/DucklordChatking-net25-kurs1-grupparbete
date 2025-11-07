e1caeb53329798102a8b96d7252cf1abb206cf2b
discovicke <discovicke@gmail.com>
2025-11-07 14:01:02 +0100

    Add LoginScreen.cs

---
+++ ChatClient/LoginScreen.cs
@@ -0,0 +1,10 @@
+namespace ChatClient;
+
+public class LoginScreen
+{
+    public bool Login(string username, string code)
+    {
+        // Validate user with server
+        return false;
+    }
+}
