40f469ccb11c42b4ed50c5f542925ad80dda591d
Marcus Löf <LeafMaster1@outlook.com>
2025-11-07 09:58:06 +0100

    new push

---
+++ Grupp H1[337]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Raylib_cs;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
+
+
