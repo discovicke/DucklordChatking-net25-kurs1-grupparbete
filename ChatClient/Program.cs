8ef6108f6197c8f4cdbff89a01e90575efb2c400
discovicke <discovicke@gmail.com>
2025-11-07 13:28:16 +0100

    Move MainMenu to method

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -8,26 +8,10 @@ internal static class Program
     public static void Main()
     {
         Raylib.InitWindow(800, 480, "DiscLord 1.0.0");
-
         
-
         while (!Raylib.WindowShouldClose())
         {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.Black);
-
-            Raylib.DrawText("Hej Mackan!", 12, 12, 20, Color.Black);
-            // Textruta test
-            Raylib.DrawRectangle(200, 200, 150, 120, Color.DarkGray);
-            Raylib.DrawText("Logga in", 230, 250, 20, Color.White);
-            
-            Raylib.DrawRectangle(200, 70, 150, 120, Color.DarkGray);
-            Raylib.DrawText("Creat User", 220, 125, 20, Color.White);
-
-            
-
-
-            Raylib.EndDrawing();
+            MainMenu.Run();
         }
 
         Raylib.CloseWindow();
