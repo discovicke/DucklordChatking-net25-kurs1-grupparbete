6e8eb97bd9ed9a0c0d69281f8791ede6340a5365
Marcus Löf <LeafMaster1@outlook.com>
2025-11-07 15:20:04 +0100

    Added class for left mouse click  ( Test )

---
+++ ChatClient/MouseInput.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace ChatClient
+{
+    internal static class MouseInput
+    {
+        private static Vector2 _mousePos;
+
+        public static void Update() 
+        {
+            _mousePos = Raylib.GetMousePosition();
+        }
+
+        // mouse position
+        public static Vector2 Position => _mousePos;
+
+        // True if mouse curser are over rectangle
+        public static bool IsHovered(Rectangle rect)
+        {
+            return Raylib.CheckCollisionPointRec(_mousePos, rect);
+        }
+
+        // True if mouse click is left when over rectangle 
+        public static bool IsLeftClick(Rectangle rect)
+        {
+            return IsHovered(rect) && Raylib.IsMouseButtonPressed(MouseButton.Left);
+        }
+
+        // colore change if mouse hoovers over rectangle
+        public static Color ButtonColor(Rectangle rect, Color hoverColor, Color normalColor)
+        {
+            return IsHovered(rect) ? hoverColor : normalColor;
+        }
+
+    }
+}
