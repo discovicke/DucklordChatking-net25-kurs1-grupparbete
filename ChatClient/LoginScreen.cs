c42a7a8011d1a2757bc6370bec491b8de0484bde
discovicke <discovicke@gmail.com>
2025-11-07 14:20:06 +0100

    Add Login communication with LoginDTO

--- ChatClient/LoginScreen.cs
+++ ChatClient/LoginScreen.cs
@@ -1,10 +1,49 @@
+using System.Net.Http.Json;
+using Shared;
+
 namespace ChatClient;
 
 public class LoginScreen
 {
-    public bool Login(string username, string code)
+    //TODO: Kolla så login fungerar med servern
+    private static readonly HttpClient httpClient = new HttpClient
     {
-        // Validate user with server
-        return false;
+        BaseAddress = new Uri("http://localhost:5000")
+    };
+
+    public static void LoginTest()
+    {
+        var loginScreen = new LoginScreen();
+        var user = new UserAccount();
+        user.Username = "Ducklord";
+        user.Code = "chatking";
+        bool loginSuccess = loginScreen.Login(user); //TODO: Läsa av input från användaren och skicka till Login-metoden
+        if (!loginSuccess)
+        {
+            Console.WriteLine("Login failed");
+        }
+        else
+        {
+            Console.WriteLine($"Login successful: {loginSuccess}");
+        }
+    }
+    
+    public bool Login(UserAccount user)
+    {
+        var loginDto = new LoginDTO
+        {
+            Username = user.Username,
+            Password = user.Code
+        };
+
+        try
+        {
+            var response = httpClient.PostAsJsonAsync("/login", loginDto).Result;
+            return response.IsSuccessStatusCode;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
