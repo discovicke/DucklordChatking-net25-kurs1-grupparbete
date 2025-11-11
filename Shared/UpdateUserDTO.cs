aba2d23e32d35aa7e317feda39f34b30625cba3c
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-11 13:36:18 +0100

    create UpdateUserDTO for updating user endpoint, and implement update user endpoint

---
+++ Shared/UpdateUserDTO.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Shared;
+
+public class UpdateUserDTO
+{
+  public string? OldUsername { get; set; }
+  public string? NewUsername { get; set; }
+  public string? Password { get; set; }
+}
