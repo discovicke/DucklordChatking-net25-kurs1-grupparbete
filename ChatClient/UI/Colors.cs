420c6cdf8c8037bf4cfc93a0f62e073fa1fc9f59
discovicke <discovicke@gmail.com>
2025-11-14 10:56:45 +0100

    Add bigger color range

--- ChatClient/UI/Colors.cs
+++ ChatClient/UI/Colors.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Raylib_cs;
+﻿using Raylib_cs;
 
 namespace ChatClient.UI
 {
@@ -11,17 +6,29 @@ namespace ChatClient.UI
     {
         // App background color
         public static Color BackgroundColor = new Color(15, 112, 152);
+        
+        // Outline color (Borders, focus, etc)
+        public static Color OutlineColor = new Color(8, 60, 80, 255); // Dark teal
+        
+        // Panel color (boxes, menus, etc)
+        public static Color PanelColor = new Color(20, 90, 120, 255); // Darkened teal
 
-        // Textfields, buttons, text in app
-        public static Color TextFieldColor = Color.Gold;
+        // TextField colors
+        public static Color TextFieldSelected = new Color(235, 245, 247, 255);
+        public static Color TextFieldHovered = new Color(245, 250, 252, 255);
+        public static Color TextFieldUnselected = new Color(210, 235, 240, 255);
+        
+        // Button colors
+        public static Color ButtonDefault = new Color(210, 150, 40, 255);
+        public static Color ButtonHovered = new Color(230, 170, 70, 255);
+        public static Color ButtonPressed = new Color(180, 120, 30, 255);
+        
+        // Success / Accent / Highlights color
+        public static Color AccentColor = Color.Gold;
 
-        // Color when hovering textfields
-        public static Color HoverColor = new Color(247, 227, 182);
-
-        // Color of text by user
-        public static Color TextColor = Color.Black;
-
-        // Subtle text color that blends in with the background
+        // Text colors
+        public static Color TextColor = new Color(20, 40, 50, 255);
+        public static Color PlaceholderText = new Color(140, 170, 180, 255);
         public static Color SubtleText = new Color(10, 70, 95);
     }
 }
