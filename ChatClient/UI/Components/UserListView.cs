b21172c647086fdefa4ccf94da244a5625a66256
discovicke <discovicke@gmail.com>
2025-11-17 21:15:09 +0100

    Renamed private variable

--- ChatClient/UI/Components/UserListView.cs
+++ ChatClient/UI/Components/UserListView.cs
@@ -8,20 +8,14 @@ namespace ChatClient.UI.Components;
 /// <summary>
 /// Responsible for: rendering online/offline user lists in a scrollable panel.
 /// </summary>
-public class UserListView
+public class UserListView(ScrollablePanel panel)
 {
-    private readonly ScrollablePanel _panel;
-    private Rectangle _bounds;
-
-    public UserListView(ScrollablePanel panel)
-    {
-        _panel = panel;
-    }
+    private Rectangle bounds;
 
     public void SetBounds(Rectangle bounds)
     {
-        _bounds = bounds;
-        _panel.SetBounds(bounds);
+        this.bounds = bounds;
+        panel.SetBounds(bounds);
     }
 
     public void Render(IReadOnlyList<string> onlineUsers, IReadOnlyList<string> offlineUsers)
@@ -31,21 +25,21 @@ public class UserListView
         const float inset = 10f;
 
         float totalHeight = lineH + (onlineUsers.Count * lineH) + 10 + lineH + (offlineUsers.Count * lineH);
-        _panel.BeginScroll(totalHeight);
+        panel.BeginScroll(totalHeight);
 
-        float x = _bounds.X + inset;
-        float y = _bounds.Y + inset;
+        float x = bounds.X + inset;
+        float y = bounds.Y + inset;
 
         // Online header
-        float scrolledY = _panel.GetScrolledY(y);
+        float scrolledY = panel.GetScrolledY(y);
         Raylib.DrawTextEx(ResourceLoader.BoldFont, "ONLINE", new Vector2(x, scrolledY), fontSize, 0.5f, Colors.AccentColor);
         y += lineH;
 
         // Online users
         foreach (var user in onlineUsers)
         {
-            scrolledY = _panel.GetScrolledY(y);
-            if (_panel.IsVisible(scrolledY, lineH))
+            scrolledY = panel.GetScrolledY(y);
+            if (panel.IsVisible(scrolledY, lineH))
             {
                 Raylib.DrawCircle((int)x + 5, (int)scrolledY + 7, 4f, Colors.AccentColor);
                 Raylib.DrawTextEx(ResourceLoader.RegularFont, user, new Vector2(x + 15, scrolledY), fontSize, 0.5f, Colors.UiText);
@@ -56,15 +50,15 @@ public class UserListView
         y += 10;
 
         // Offline header
-        scrolledY = _panel.GetScrolledY(y);
+        scrolledY = panel.GetScrolledY(y);
         Raylib.DrawTextEx(ResourceLoader.BoldFont, "OFFLINE", new Vector2(x, scrolledY), fontSize, 0.5f, Colors.SubtleText);
         y += lineH;
 
         // Offline users
         foreach (var user in offlineUsers)
         {
-            scrolledY = _panel.GetScrolledY(y);
-            if (_panel.IsVisible(scrolledY, lineH))
+            scrolledY = panel.GetScrolledY(y);
+            if (panel.IsVisible(scrolledY, lineH))
             {
                 Raylib.DrawCircle((int)x + 5, (int)scrolledY + 7, 4f, Colors.SubtleText);
                 Raylib.DrawTextEx(ResourceLoader.RegularFont, user, new Vector2(x + 15, scrolledY), fontSize, 0.5f, Colors.SubtleText);
@@ -72,7 +66,7 @@ public class UserListView
             y += lineH;
         }
 
-        _panel.EndScroll();
+        panel.EndScroll();
     }
 }
 
